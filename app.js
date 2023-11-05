5f75e2afb69eb3a8455097a9b40bfffb9d542bc5
Mokheon Park <mokimokheonpark@gmail.com>
2023-11-04 22:31:53 -0400

    implemented image-upload using AWS S3

--- app.js
+++ app.js
@@ -66,6 +66,27 @@ passport.deserializeUser(async (user, done) => {
   });
 });
 
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const s3 = new S3Client({
+  region: "us-east-2",
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_KEY_ID,
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "moki1forum",
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
 const { MongoClient, ObjectId } = require("mongodb");
 
 let db;
@@ -86,31 +107,23 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/list", async (req, res) => {
-  try {
-    let data = await db.collection("post").find().toArray();
-    res.render("list.ejs", { posts: data });
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
 app.get("/write", (req, res) => {
   res.render("write.ejs");
 });
 
-app.post("/write-post", async (req, res) => {
+app.post("/write-post", upload.single("img1"), async (req, res) => {
   try {
     if (req.body.title === "") {
       res.send("Please write the title");
     } else if (req.body.content === "") {
       res.send("Please write the content");
     } else {
-      await db
-        .collection("post")
-        .insertOne({ title: req.body.title, content: req.body.content });
-      res.redirect("/list");
+      await db.collection("post").insertOne({
+        title: req.body.title,
+        content: req.body.content,
+        img: req.file.location,
+      });
+      res.redirect("/list/1");
     }
   } catch (e) {
     console.log(e);
