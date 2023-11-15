9bc8d917ce28cd16e4a5f174334aafe9ade131c5
Mokheon Park <mokimokheonpark@gmail.com>
2023-11-15 16:41:17 -0500

    exception and authorization handling in chat/detail

--- routes/chat.js
+++ routes/chat.js
@@ -12,26 +12,48 @@ connectDB
   });
 
 router.get("/request", async (req, res) => {
-  await db.collection("chatroom").insertOne({
-    participants: [req.user._id, new ObjectId(req.query.postUser)],
-    date: new Date(),
-  });
-  res.redirect("/chat/list");
+  try {
+    await db.collection("chatroom").insertOne({
+      participants: [req.user._id, new ObjectId(req.query.postUser)],
+      date: new Date(),
+    });
+    res.redirect("/chat/list");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 router.get("/list", async (req, res) => {
-  let data = await db
-    .collection("chatroom")
-    .find({ participants: req.user._id })
-    .toArray();
-  res.render("chatList.ejs", { chatList: data, user: req.user });
+  try {
+    let data = await db
+      .collection("chatroom")
+      .find({ participants: req.user._id })
+      .toArray();
+    res.render("chatList.ejs", { chatList: data, user: req.user });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 router.get("/detail/:id", async (req, res) => {
-  let data = await db
-    .collection("chatroom")
-    .findOne({ _id: new ObjectId(req.params.id) });
-  res.render("chatDetail.ejs", { chatDetail: data, user: req.user });
+  try {
+    let data = await db
+      .collection("chatroom")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    if (
+      req.user._id.toString() === data.participants[0].toString() ||
+      req.user._id.toString() === data.participants[1].toString()
+    ) {
+      res.render("chatDetail.ejs", { chatDetail: data, user: req.user });
+    } else {
+      res.status(403).send("Forbidden");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
